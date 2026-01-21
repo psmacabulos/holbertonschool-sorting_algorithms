20fa6f51b084d6702d9c87bdaf093df39f9a64f7
psmacabulos <patrick.macabulos@gmail.com>
2026-01-21 08:42:29 +0800

    Initialize sorting_algorithms project structure

---
+++ sort.h
@@ -0,0 +1,29 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element
+* @next: Pointer to the next element
+*/
+typedef struct listint_s
+{
+	const int n;
+
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+
+/* Provided printing functions */
+void print_array(const int *array, size_t size);
+
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
